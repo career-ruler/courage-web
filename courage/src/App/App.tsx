73d859f86142d982fdea3235f4b70165f735fbbb
leeyoonchae <yoonchae2015@gmail.com>
2025-03-24 13:48:36 +0900

    ➕ 의존성 및 파일 추가

---
+++ courage/src/App/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import AppRouter from "components/Router/AppRouter";
+import GlobalStyle from "styles/global";
+
+function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <AppRouter />
+    </>
+  );
+}
+
+export default App;
