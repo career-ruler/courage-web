36c790f5ed82b3a4512084d8f83ae81d370d1fb0
leeyoonchae <yoonchae2015@gmail.com>
2025-04-09 12:06:56 +0900

    💄 회원가입 페이지

--- courage/src/components/Auth/index.tsx
+++ courage/src/components/Auth/index.tsx
@@ -2,10 +2,32 @@ import React from "react";
 import * as S from "./style";
 
 const Auth = () => {
-    return (
+  return (
+    <S.AuthContainer>
       <S.TitleText>회원가입</S.TitleText>
-    );
-  };
-  
+      <S.discriptionText>
+        매일 새로운 소식과 함께
+        <br />
+        커리지의 다양한 면접을 경험해 보세요!
+      </S.discriptionText>
+      <S.InputFieldContainer>
+          <S.TitleTextField>아이디</S.TitleTextField>
+          <S.InputField type="text" placeholder="아이디를 입력하세요" />
+        </S.InputFieldContainer>
+
+      <S.InputFieldContainer>
+        <S.TitleTextField>비밀번호</S.TitleTextField>
+        <S.InputField type="password" placeholder="비밀번호를 입력하세요" />
+      </S.InputFieldContainer>
+
+      <S.InputFieldContainer>
+        <S.TitleTextField>비밀번호 확인</S.TitleTextField>
+        <S.InputField type="password" placeholder="비밀번호를 재입력하세요" />
+      </S.InputFieldContainer>
+
+      <S.SignupButton>회원가입</S.SignupButton>
+    </S.AuthContainer>
+  );
+};
 
 export default Auth;
--- courage/src/components/Auth/style.ts
+++ courage/src/components/Auth/style.ts
@@ -1,39 +1,75 @@
 import styled from "styled-components";
 
-export const SignInButton = styled.button`
-    width: 350px;
-    height: 55px;
-    background-color: white;
-    color: #626161;
-    font-size: 21px;
-    font-weight: 600;
-    border-radius: 15px;
-    border: 1px solid #C4C1C1;
-    display: flex;
-    justify-content: center;
-    align-items: center;
+export const AuthContainer = styled.div`
+  width: 55%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #fff;
+  padding: 20px;
+  margin: 0 auto;
 `;
 
 export const TitleText = styled.p`
-    color: #fff;
-    font-size: 25px;
-    font-weight: 600;
-    
+  color: #000;
+  font-size: 25px;
+  font-weight: 600;
 `;
 
-export const SignInContainer = styled.div`
-    width: auto;
-    height: 100%;
-    
+export const discriptionText = styled.p`
+  color: #939191;
+  font-size: 15px;
+  font-weight: 400;
+  margin-top: 10px;
+  margin-bottom: 30px;
+  text-align: center;
 `;
-// export const HeaderBarContainer = styled.div`
-//     width: 100%;
-//     min-width: 260px;
-//     height: 116px;
-    
-//     display: flex;
-//     align-items: center;
-//     background: #FFF;
-//     padding: 0 20px;
-//     justify-content: space-between;
-// `;
+export const InputFieldContainer = styled.div`
+  width: 60%;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: center;
+  margin-bottom: 20px;
+`;
+
+export const TitleTextField = styled.p`
+  color: #000000;
+  font-size: 14px;
+  font-weight: 600;
+  margin-bottom: 5px;
+`;
+
+export const InputField = styled.input`
+  width: 100%;
+  height: 40px;
+  padding: 10px;
+  font-size: 16px;
+  color: #000000;
+  border: 1px solid #fff;
+  border-radius: 5px;
+  outline: none;
+  background-color: #f6f5f8;
+  margin-bottom: 8px;
+
+  ::placeholder {
+    color: #aaaaaa;
+    font-size: 14px;
+  }
+`;
+
+export const SignupButton = styled.button`
+  width: 60%;
+  height: 40px;
+  margin-top: 45px;
+  margin-bottom: 100px;
+  background-color: #313030;
+  color: #ffffff;
+  font-size: 14px;
+  font-weight: bold;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+`;
